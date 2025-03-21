85d9b9acbec4ba67b674b26b0536ef008c633a65
Vingtor Ubuntu <odinson.vingtor@gmail.com>
2025-03-21 03:23:53 -0300

    Rápidas alterações de nova config

--- CMakeFiles/GBEmu.dir/DependInfo.cmake
+++ CMakeFiles/GBEmu.dir/DependInfo.cmake
@@ -8,19 +8,15 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/glauco/Desktop/random/GBEmu/src/defs/CPU.cpp" "CMakeFiles/GBEmu.dir/src/defs/CPU.cpp.o" "gcc" "CMakeFiles/GBEmu.dir/src/defs/CPU.cpp.o.d"
-  "/home/glauco/Desktop/random/GBEmu/src/defs/Memoria/Memory.cpp" "CMakeFiles/GBEmu.dir/src/defs/Memoria/Memory.cpp.o" "gcc" "CMakeFiles/GBEmu.dir/src/defs/Memoria/Memory.cpp.o.d"
-  "/home/glauco/Desktop/random/GBEmu/src/defs/Memoria/MemoryParts.cpp" "CMakeFiles/GBEmu.dir/src/defs/Memoria/MemoryParts.cpp.o" "gcc" "CMakeFiles/GBEmu.dir/src/defs/Memoria/MemoryParts.cpp.o.d"
-  "/home/glauco/Desktop/random/GBEmu/src/defs/ROM/ROMLoader.cpp" "CMakeFiles/GBEmu.dir/src/defs/ROM/ROMLoader.cpp.o" "gcc" "CMakeFiles/GBEmu.dir/src/defs/ROM/ROMLoader.cpp.o.d"
-  "/home/glauco/Desktop/random/GBEmu/src/main.cpp" "CMakeFiles/GBEmu.dir/src/main.cpp.o" "gcc" "CMakeFiles/GBEmu.dir/src/main.cpp.o.d"
+  "/home/glauco/Desktop/GBEmulador/src/defs/CPU.cpp" "CMakeFiles/GBEmu.dir/src/defs/CPU.cpp.o" "gcc" "CMakeFiles/GBEmu.dir/src/defs/CPU.cpp.o.d"
+  "/home/glauco/Desktop/GBEmulador/src/defs/Memoria/Memory.cpp" "CMakeFiles/GBEmu.dir/src/defs/Memoria/Memory.cpp.o" "gcc" "CMakeFiles/GBEmu.dir/src/defs/Memoria/Memory.cpp.o.d"
+  "/home/glauco/Desktop/GBEmulador/src/defs/Memoria/MemoryParts.cpp" "CMakeFiles/GBEmu.dir/src/defs/Memoria/MemoryParts.cpp.o" "gcc" "CMakeFiles/GBEmu.dir/src/defs/Memoria/MemoryParts.cpp.o.d"
+  "/home/glauco/Desktop/GBEmulador/src/defs/ROM/ROMLoader.cpp" "CMakeFiles/GBEmu.dir/src/defs/ROM/ROMLoader.cpp.o" "gcc" "CMakeFiles/GBEmu.dir/src/defs/ROM/ROMLoader.cpp.o.d"
+  "/home/glauco/Desktop/GBEmulador/src/main.cpp" "CMakeFiles/GBEmu.dir/src/main.cpp.o" "gcc" "CMakeFiles/GBEmu.dir/src/main.cpp.o.d"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
