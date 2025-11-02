463a3b0e64497ba8babd7d562f149af5274e1a5c
Odinson-Vingtor-ArchLinux <odinson.vingtor@gmail.com>
2025-11-02 16:25:23 -0300

    Adds a default read/write method to bus, allowing to read/write to memory without explicity informing the Device

--- src/defs/CPU/Bus.cpp
+++ src/defs/CPU/Bus.cpp
@@ -25,3 +25,31 @@ void Bus::write(const DeviceEnum &device, const uint16_t &address, const uint8_t
 
     return devicePtr->second->write(address, value);
 }
+
+uint8_t Bus::read(const uint16_t &address) {
+    /***
+     *Default method reads from memory
+     */
+
+    auto memoryPtr = devices.find(DeviceEnum::Memory);
+
+    if(memoryPtr == devices.end()) {
+        throw std::runtime_error("It was not possible to find a memory device." );
+    }
+
+    return memoryPtr->second->read(address);
+}
+
+void Bus::write(const uint16_t &address, const uint8_t &value) {
+    /***
+    *Default method writes to memory
+    */
+
+    auto memoryPtr = devices.find(DeviceEnum::Memory);
+
+    if(memoryPtr == devices.end()) {
+        throw std::runtime_error("Tried to access a undefined Device." );
+    }
+
+    return memoryPtr->second->write(address, value);
+}
